193f4be8d9fb838f72ce6bcfbdb0032bd6b6837e
jimli4556 <506135224@qq.com>
2019-05-09 14:46:21 +0800

    添加vuex的使用

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,19 +2,41 @@
   <div class="m-box">
     <p>{{msg}}</p>
 		<h1>{{this.$store.state.count}}</h1>
+		<h2>这是getters中获取到的数据,{{this.$store.getters.getStateCount}}</h2>
+		<button @click="addfun">+</button>
+		<button @click="reductfun">-</button>
+		<h3>{{count1}}</h3>
   </div>
 </template>
 
 <script>
+	import{mapState,mapActions,mapGetters} from 'vuex';
 export default {
   name: 'HelloWorld',
   props: {
     msg: String
-  }
+  },
+	computed: {
+		...mapState({
+			count1:state=>state.count
+		})
+	},
+	methods: {
+		addfun() {
+			// 	this.$store.commit("add")
+			this.$store.dispatch("addfun")
+		},
+		reductfun() {
+			// 	this.$store.commit("reduction")
+			this.$store.dispatch("reductfun")
+		}
+	},
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-	
+	h1,h2{
+		color: #fff;
+	}
 </style>
--- src/store.js
+++ src/store.js
@@ -7,10 +7,25 @@ export default new Vuex.Store({
   state: {
 		count:2
   },
+	getters: {
+			getStateCount:function(state){
+				return state.count+1;
+			}
+    },
   mutations: {
-
+		add(state){
+			state.count=state.count+1;
+		},
+		reduction(state){
+			state.count=state.count-1;
+		}
   },
   actions: {
-
+		addfun(context){
+			context.commit("add");
+		},
+		reductfun(context){
+			context.commit("reduction");
+		}
   }
 })
